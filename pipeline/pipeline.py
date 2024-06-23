58c29c6865e4925a30aabdd7afff738c70864d62
Akshay <akshaygupta533@gmail.com>
2024-06-23 16:14:42 +0530

    Reformatting python files to pass ci checks

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,20 +1,22 @@
-from scraper import WebScraper
-from database import DataBase
-from logger import make_logger
+import time
+
 import schedule
 from analysis import DatasetAnalyzer
-import time
+from database import DataBase
+from logger import make_logger
+from scraper import WebScraper
 
 log = make_logger("pipeline")
 # Global variable to track if first pipeline has completed successfully
 first_pipeline_completed = False
+
 
 def scrape_data_and_get_scores():
     global first_pipeline_completed
     # Create the WebDriver instance
     scraper = WebScraper()
     first_pipeline_completed = False
-    
+
     log.info("Fetching top 5 links from both sources")
     yourstory_links = scraper.get_yourstory_top_5("tata motors")
     finshots_links = scraper.get_finshots_top_5("tata motors")
@@ -22,22 +24,23 @@ def scrape_data_and_get_scores():
     for link in yourstory_links:
         log.info(f"Getting score for {link}")
         score = scraper.get_yourstory_article_sentiment_score(url=link)
-        scores_1.append({'url': link, 'score': score})
+        scores_1.append({"url": link, "score": score})
     scores_2 = []
     for link in finshots_links:
         log.info(f"Getting score for {link}")
         score = scraper.get_finshots_article_sentiment_score(url=link)
-        scores_2.append({'url': link, 'score': score})
-            
+        scores_2.append({"url": link, "score": score})
+
     # Close the WebDriver instance
     scraper.close_driver()
     log.info("Writing scores to sqlite database")
-    #Write the sentiment score to a local sqlite database
+    # Write the sentiment score to a local sqlite database
     db = DataBase()
-    db.insert_data(scores_1+scores_2)
+    db.insert_data(scores_1 + scores_2)
     db.close_conn()
 
     first_pipeline_completed = True
+
 
 def analyse_100k_dataset():
 
@@ -45,25 +48,29 @@ def analyse_100k_dataset():
     if first_pipeline_completed:
         log.info("Starting second pipeline")
         analyzer = DatasetAnalyzer()
-        #Task 1
+        # Task 1
         log.info("Doing task 1")
-        mean_occupation_age = analyzer.get_occupation_mean_age()
-        #Task 2
+        mean_occupation_age = analyzer.get_occupation_mean_age()  # noqa: F841
+        # Task 2
         log.info("Doing task 2")
-        top_20_movies = analyzer.get_top_n_movies(20,35)
-        #Task 3
+        top_20_movies = analyzer.get_top_n_movies(20, 35)  # noqa: F841
+        # Task 3
         log.info("Doing task 3")
-        top_genres_occupation_and_age_group = analyzer.get_top_genres_occupation_and_age_group()
-        #Task 4
+        top_genres_occupation_and_age_group = (  # noqa: F841
+            analyzer.get_top_genres_occupation_and_age_group()
+        )
+        # Task 4
     else:
-        log.info("Skipping second pipeline because the first failed or did not complete.")
+        log.info(
+            "Skipping second pipeline because the first failed or did not complete."
+        )
 
 
 # Schedule the first method to run at 7PM
-schedule.every().day.at("10:02").do(scrape_data_and_get_scores) #pipeline 1
-schedule.every().day.at("10:04").do(analyse_100k_dataset) #pipeline 2
+schedule.every().day.at("10:02").do(scrape_data_and_get_scores)  # pipeline 1
+schedule.every().day.at("10:04").do(analyse_100k_dataset)  # pipeline 2
 
-if __name__=='__main__':
+if __name__ == "__main__":
     # Main loop to run the scheduler
     log.info("Scheduling pipelines to run")
     while True:
--- pipeline/scraper.py
+++ pipeline/scraper.py
@@ -1,17 +1,13 @@
-import os
-from selenium import webdriver
-from urllib.parse import quote
-from selenium.webdriver.common.by import By
-import time
 import re
+import time
+from urllib.parse import quote
+
+import requests
+from logger import make_logger
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import nltk
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import requests
-
-from logger import make_logger
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # SELENIUM_SERVER_URL = os.environ["SELENIUM_SERVER_URL"]
 SELENIUM_SERVER_URL = "http://localhost:4444/wd/hub"
@@ -21,93 +17,102 @@ FINSHOTS_URL = "https://finshots.in"
 
 log = make_logger("pipeline")
 
+
 class WebScraper:
-    def __init__(self,retries=5):
+    def __init__(self, retries=5):
         # URL of the remote WebDriver server
-        
+
         selenium_server_url = SELENIUM_SERVER_URL
-        
+
         options = webdriver.FirefoxOptions()
-        
+
         for _ in range(retries):
             try:
                 self.driver = webdriver.Remote(
-                    command_executor = selenium_server_url,
-                    options = options
+                    command_executor=selenium_server_url, options=options
                 )
                 break
-            except:
+            except Exception as e:
                 log.error("Could not connect to selenium server")
+                log.error(e)
                 log.error("Retrying....")
                 time.sleep(2)
         self.driver.implicitly_wait(5)
-    
+
     def reinit_driver(self):
         self.driver.quit()
         selenium_server_url = SELENIUM_SERVER_URL
-        
+
         options = webdriver.FirefoxOptions()
-        
+
         self.driver = webdriver.Remote(
-            command_executor = selenium_server_url,
-            options = options
+            command_executor=selenium_server_url, options=options
         )
         self.driver.implicitly_wait(5)
 
-    def clean_text(self,text):
-        text = re.sub(r'[^a-zA-Z\s]', '', text) #Clean special characters
-        text = text.lower() #Convert to lowercase
-        tokens = text.split() #Tokenize text
-        stop_words = set(stopwords.words('english'))
+    def clean_text(self, text):
+        text = re.sub(r"[^a-zA-Z\s]", "", text)  # Clean special characters
+        text = text.lower()  # Convert to lowercase
+        tokens = text.split()  # Tokenize text
+        stop_words = set(stopwords.words("english"))
         filtered_tokens = [token for token in tokens if token not in stop_words]
         lemmatizer = WordNetLemmatizer()
         lemmtized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
-        clean_text = ' '.join(lemmtized_tokens)
+        clean_text = " ".join(lemmtized_tokens)
         return clean_text
 
-    def get_yourstory_top_5(self,keyword):
-        url = YOURSTORY_SEARCH_URL.replace("KEYWORD",quote(keyword))
+    def get_yourstory_top_5(self, keyword):
+        url = YOURSTORY_SEARCH_URL.replace("KEYWORD", quote(keyword))
 
         self.driver.get(url)
-        elements = self.driver.find_elements(By.XPATH, "//div//li//span[@pathname='/search']")[:5]
+        elements = self.driver.find_elements(
+            By.XPATH, "//div//li//span[@pathname='/search']"
+        )[:5]
         links = [
-            el.find_element(By.XPATH,"./ancestor::a").get_attribute('href') for el in elements
+            el.find_element(By.XPATH, "./ancestor::a").get_attribute("href")
+            for el in elements
         ]
         return links
 
-    def get_yourstory_article_sentiment_score(self,url):
-        #re-init the webdriver to prevent blocking from website
+    def get_yourstory_article_sentiment_score(self, url):
+        # re-init the webdriver to prevent blocking from website
         self.reinit_driver()
         self.driver.get(url)
-        article_div = self.driver.find_element(By.XPATH, "//div[@id='article_container']")
+        article_div = self.driver.find_element(
+            By.XPATH, "//div[@id='article_container']"
+        )
         article_text = article_div.text
         cleaned_text = self.clean_text(article_text)
-        res = requests.post(f"{DUMMY_API_URL}/get-senti-score/",json = {"text":cleaned_text})
-        return res.json()['result']
+        res = requests.post(
+            f"{DUMMY_API_URL}/get-senti-score/", json={"text": cleaned_text}
+        )
+        return res.json()["result"]
 
-    def get_finshots_article_sentiment_score(self,url):
+    def get_finshots_article_sentiment_score(self, url):
         self.driver.get(url)
         article_div = self.driver.find_element(By.XPATH, "//div[@class='post-content']")
         article_text = article_div.text
         cleaned_text = self.clean_text(article_text)
-        res = requests.post(f"{DUMMY_API_URL}/get-senti-score/",json = {"text":cleaned_text})
-        return res.json()['result']
-        
+        res = requests.post(
+            f"{DUMMY_API_URL}/get-senti-score/", json={"text": cleaned_text}
+        )
+        return res.json()["result"]
 
-    def get_finshots_top_5(self,keyword):
+    def get_finshots_top_5(self, keyword):
         url = FINSHOTS_URL
         self.driver.get(url)
         search_class_name = "toggle-search-button"
-        search_button = self.driver.find_element(By.CSS_SELECTOR, f"a.{search_class_name}")
+        search_button = self.driver.find_element(
+            By.CSS_SELECTOR, f"a.{search_class_name}"
+        )
         search_button.click()
-        input_field = self.driver.find_element(By.XPATH,"//input[@type='search']")
+        input_field = self.driver.find_element(By.XPATH, "//input[@type='search']")
         input_field.send_keys(keyword)
-        article_elements = self.driver.find_elements(By.XPATH, "//a[@class='c-search-result']")[:5]
-        links = [
-            el.get_attribute('href') for el in article_elements
-        ]
+        article_elements = self.driver.find_elements(
+            By.XPATH, "//a[@class='c-search-result']"
+        )[:5]
+        links = [el.get_attribute("href") for el in article_elements]
         return links
-    
+
     def close_driver(self):
         self.driver.quit()
-
