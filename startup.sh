7886c64a540993e4792d1116677170a9847c832f
Akshay <akshaygupta533@gmail.com>
2024-06-24 17:46:11 +0530

    Added Readme file

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ export SELENIUM_SERVER_URL="http://host.docker.internal:4444/wd/hub"
 export YOURSTORY_SEARCH_URL="https://yourstory.com/search?q=KEYWORD&page=1"
 export FINSHOTS_URL="https://finshots.in"
 export TICKER_LIST="hyundai|tata motors"
-export FIRST_PIPELINE_TIME="16:46" 
-export SECOND_PIPELINE_TIME="16:42"
+export FIRST_PIPELINE_TIME="19:00" 
+export SECOND_PIPELINE_TIME="20:00"
 export MOVIE_ID="1" #Movie id for pipeline 2, task 4
 
 docker compose up
